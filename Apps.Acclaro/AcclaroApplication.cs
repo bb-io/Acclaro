76f1445a47e43a1a2eaaeef54aba88d2a868d15e
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 12:25:55 +0300

    added categories

--- Apps.Acclaro/AcclaroApplication.cs
+++ Apps.Acclaro/AcclaroApplication.cs
@@ -1,9 +1,17 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Acclaro
 {
-    public class AcclaroApplication : IApplication
+    public class AcclaroApplication : IApplication, ICategoryProvider
     {
+        
+        public IEnumerable<ApplicationCategory> Categories
+        {
+            get => [ApplicationCategory.LspPortal];
+            set { }
+        }
+        
         public string Name
         {
             get => "My Acclaro";
